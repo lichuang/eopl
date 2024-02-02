034e7ff0d25b2fbf39ed5d5bf60c3683c52a5a3e
codedump <lichuang1982@gmail.com>
2024-02-02 20:54:53 +0800

    ci

---
+++ ch03/3.11.rkt
@@ -0,0 +1,8 @@
+#lang racket
+#|
+Exercise 3.11 [*] In a real language, one might have many operators such as those 
+  in the preceding exercises. Rearrange the code in the interpreter so that 
+  it is easy to add new operators.
+|#
+
+; see let-lang.rkt
